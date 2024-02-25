50ea34778b142e67b6377cc19b31100402e2f11f
L1nker2 <l1nkerz12344321@gmail.com>
2024-02-25 20:42:17 +0500

    fix design

--- Client/Katalog.Designer.cs
+++ Client/Katalog.Designer.cs
@@ -28,85 +28,158 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.panel3 = new System.Windows.Forms.Panel();
             this.panel1 = new System.Windows.Forms.Panel();
-            this.button1 = new System.Windows.Forms.Button();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label1 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel3.SuspendLayout();
+            this.panel4.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             this.SuspendLayout();
-            // 
-            // panel3
-            // 
-            this.panel3.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.panel3.Location = new System.Drawing.Point(958, 87);
-            this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(300, 400);
-            this.panel3.TabIndex = 2;
             // 
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.SystemColors.ActiveBorder;
             this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.panel1.Controls.Add(this.button1);
             this.panel1.Controls.Add(this.label2);
             this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.button1);
             this.panel1.Controls.Add(this.pictureBox1);
-            this.panel1.Location = new System.Drawing.Point(515, 87);
+            this.panel1.Location = new System.Drawing.Point(316, 10);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(300, 400);
+            this.panel1.Size = new System.Drawing.Size(270, 350);
             this.panel1.TabIndex = 1;
             // 
-            // button1
+            // panel2
             // 
-            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(18)))), ((int)(((byte)(53)))));
-            this.button1.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.button1.ForeColor = System.Drawing.Color.White;
-            this.button1.Location = new System.Drawing.Point(50, 340);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(200, 50);
-            this.button1.TabIndex = 5;
-            this.button1.Text = "В корзину";
-            this.button1.UseVisualStyleBackColor = false;
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label2.Location = new System.Drawing.Point(10, 290);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(52, 24);
-            this.label2.TabIndex = 4;
-            this.label2.Text = "label2";
-            // 
-            // label1
-            // 
-            this.label1.AutoEllipsis = true;
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label1.Location = new System.Drawing.Point(10, 220);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(52, 24);
-            this.label1.TabIndex = 3;
-            this.label1.Text = "label1";
+            this.panel2.Location = new System.Drawing.Point(10, 10);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(400, 554);
+            this.panel2.TabIndex = 2;
             // 
             // pictureBox1
             // 
             this.pictureBox1.Location = new System.Drawing.Point(3, 3);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(294, 200);
+            this.pictureBox1.Size = new System.Drawing.Size(262, 200);
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(18)))), ((int)(((byte)(53)))));
+            this.button1.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(75, 305);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(120, 40);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "В корзину";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoEllipsis = true;
+            this.label1.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.label1.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(3, 216);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(262, 53);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Название товара";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(3, 269);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(107, 24);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Цена товара";
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.panel4);
+            this.panel3.Controls.Add(this.panel1);
+            this.panel3.Location = new System.Drawing.Point(416, 10);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(902, 552);
+            this.panel3.TabIndex = 3;
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.panel4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel4.Controls.Add(this.label3);
+            this.panel4.Controls.Add(this.label4);
+            this.panel4.Controls.Add(this.button2);
+            this.panel4.Controls.Add(this.pictureBox2);
+            this.panel4.Location = new System.Drawing.Point(622, 10);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(270, 350);
+            this.panel4.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(3, 269);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(107, 24);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Цена товара";
+            // 
+            // label4
+            // 
+            this.label4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.label4.AutoEllipsis = true;
+            this.label4.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.label4.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(3, 216);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(262, 53);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Название товара";
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(18)))), ((int)(((byte)(53)))));
+            this.button2.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.ForeColor = System.Drawing.Color.White;
+            this.button2.Location = new System.Drawing.Point(75, 305);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(120, 40);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "В корзину";
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(3, 3);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(262, 200);
+            this.pictureBox2.TabIndex = 0;
+            this.pictureBox2.TabStop = false;
             // 
             // Katalog
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1330, 574);
-            this.Controls.Add(this.panel1);
             this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "Katalog";
             this.Text = "Katalog";
@@ -114,16 +187,26 @@
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel3.ResumeLayout(false);
+            this.panel4.ResumeLayout(false);
+            this.panel4.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
-        private System.Windows.Forms.Panel panel3;
         private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox pictureBox2;
     }
 }
--- Client/Katalog.cs
+++ Client/Katalog.cs
@@ -66,37 +66,8 @@ namespace Client
                 string command = "LoadProductsPlease";
                 string sResponse = await SendRequest(server, port, command);
                 List<Product> products = JsonConvert.DeserializeObject<List<Product>>(sResponse);
-                
                 foreach (Product product in products)
                 {
-                    /*
-                    Panel panelProductCard = new Panel();
-                    panelProductCard.BorderStyle = BorderStyle.FixedSingle;
-
-                    PictureBox pictureBoxProduct = new PictureBox();
-                    pictureBoxProduct.Image = DecodeBase64Image(product.ImgBase64);
-                    pictureBoxProduct.SizeMode = PictureBoxSizeMode.StretchImage;
-                    pictureBoxProduct.Dock = DockStyle.Top;
-                    panelProductCard.Controls.Add(pictureBoxProduct);
-
-                    Label labelProductName = new Label();
-                    labelProductName.Text = product.Name;
-                    labelProductName.Dock = DockStyle.Top;
-                    panelProductCard.Controls.Add(labelProductName);
-
-                    Label labelProductPrice = new Label();
-                    labelProductPrice.Text = product.Price.ToString();
-                    labelProductPrice.Dock = DockStyle.Top;
-                    panelProductCard.Controls.Add(labelProductPrice);
-
-                    Button buttonBuy = new Button();
-                    buttonBuy.Text = "Купить";
-                    buttonBuy.Dock = DockStyle.Top;
-                    buttonBuy.Click += (sender, e) => BuyButtonClick(product.Id);
-                    panelProductCard.Controls.Add(buttonBuy);
-
-                    this.Controls.Add(panelProductCard);
-                    */
                     CreateCard(new Point(72, 87), product);
                 }
             }
@@ -105,35 +76,40 @@ namespace Client
                 MessageBox.Show(ex.Message);
             }
         }
-
+        //locations:
+        //10;10
+        //316;10
+        //622;10
+        //step = 20
         public void CreateCard(Point location, Product product)
         {
             Panel cardPanel = new Panel();
             cardPanel.Location = location;
-            cardPanel.Size = new Size(300, 400);
+            cardPanel.Size = new Size(270, 350);
             cardPanel.BorderStyle = BorderStyle.FixedSingle;
 
             // Создание картинки товара
             PictureBox imageBox = new PictureBox();
             imageBox.Parent = cardPanel;
             imageBox.Location = new Point(3, 3);
-            imageBox.Size = new Size(294, 200);
+            imageBox.Size = new Size(262, 200);
             imageBox.SizeMode = PictureBoxSizeMode.StretchImage;
             imageBox.Image = DecodeBase64Image(product.ImgBase64);
 
             // Создание названия товара
             Label nameLabel = new Label();
             nameLabel.Parent = cardPanel;
-            nameLabel.Location = new Point(10, 220);
-            nameLabel.Size = new Size(200, 100);
-            nameLabel.AutoSize = true;
+            nameLabel.Location = new Point(3, 216);
+            nameLabel.Size = new Size(262, 53);
+            nameLabel.AutoSize = false;
+            nameLabel.AutoEllipsis = true;
             nameLabel.Font = new Font("Arial", 12);
             nameLabel.Text = product.Name;
 
             // Создание цены товара
             Label priceLabel = new Label();
             priceLabel.Parent = cardPanel;
-            priceLabel.Location = new Point(10, 290);
+            priceLabel.Location = new Point(3, 269);
             priceLabel.Size = new Size(200, 25);
             priceLabel.AutoSize = true;
             priceLabel.Font = new Font("Arial", 12);
@@ -142,9 +118,9 @@ namespace Client
             // Создание кнопки добавления в корзину
             Button addButton = new Button();
             addButton.Parent = cardPanel;
-            addButton.Location = new Point(50, 340);
-            addButton.Size = new Size(200, 50);
-            addButton.Font = new Font("Arial", 12);
+            addButton.Location = new Point(75, 305);
+            addButton.Size = new Size(120, 40);
+            addButton.Font = new Font("Arial", 12, FontStyle.Bold);
             addButton.ForeColor = Color.White;
             addButton.BackColor = ColorTranslator.FromHtml("#e21235");
             addButton.Text = "В корзину";
